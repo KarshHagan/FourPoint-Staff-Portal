1b22b31a58b3068fa8b2290d649be475e7bb1606
josephsap <joesap1@gmail.com>
2016-06-24 13:20:59 -0600

    employee directory: final w/styling

--- wp-content/themes/staffportal/assets/javascripts/staff-portal.js
+++ wp-content/themes/staffportal/assets/javascripts/staff-portal.js
@@ -1,10 +1,156 @@
 $(document).ready(function() {
-
+  var windowWidth = $(window).width();
 
   // staff portal bio cards = flip on click
-  var $employeeCard = $('.employee-bio');
-  $employeeCard.on('click', function() {
-    $(this).toggleClass('flip');
+  var $flipTrigger = $('.more-flip');
+  $flipTrigger.on('click', function(e) {
+    e.preventDefault();
+    $(this).closest('.employee-bio').toggleClass('flip');
+  }); 
+
+  // alert Modal toggle
+  document.getElementById('alert-toggle').addEventListener('click', function() {
+    $('body').css('overflow-Y', 'hidden');
+    $('.alert-modal').fadeToggle(200);
+    $('.alert-list-panel').addClass('scale-in');
   });
 
+  $('.close-alert').on('click', function() {
+    $('.alert-modal').fadeToggle('fast', function() {
+      $('.alert-list-panel').removeClass('scale-in');
+      $('body').css('overflow-Y', 'scroll');
+    });
+  });
+
+
+
+  // STAFF PORTAL ACCORDIONS
+  function accordionCollapse(windowWidth) {
+    if(windowWidth <= 768) {
+      $('.collapse-item-click').on('click', function() {
+        $(this).next().slideToggle(250);
+      });
+    }
+  }
+
+  accordionCollapse(windowWidth);
+
+  $(window).on('resize', function(windowWidth) {
+    accordionCollapse(windowWidth);
+  });
+
+  var employeeSort = new EmployeeSort();
+
 });
+
+
+// sorting for the company directory
+function EmployeeSort() {
+
+  // button for office sort
+  this.$officeSelect = $('.office-btn');
+
+  // button for name sort
+  this.$nameSelect = $('.name-btn');
+
+  // item to hide/show
+  this.$sortItem = $('.employee-bio-container');
+  
+  this.sortByOffice();
+  this.sortByName();
+}
+
+// sort by office
+EmployeeSort.prototype.sortByOffice = function() {
+  var _this = this;
+
+  this.$officeSelect.on('click', function(e) {
+    e.preventDefault();
+
+    // switch active class to selected button to make blue
+    $('.office-btn').removeClass('active');
+    $(this).addClass('active');
+
+    // the name button current active
+    var nameItemActive = $('.name-sort').data('active-name');
+
+    var selectedOffice = $(this).data('office-selected');
+
+    // loop through cards
+    _this.$sortItem.each(function() {
+
+        // the name button current active
+        var itemOffice = $(this).data('office');
+
+        // if selected office (button) matches item's office and selected name (button) matches item's name OR selected office (button) matches item's office and selected name (button) is all, fade in
+        if(selectedOffice == itemOffice && nameItemActive == $(this).data('name') || selectedOffice == itemOffice && nameItemActive == 'all') {
+            $(this).addClass('card-visible').removeClass('card-hidden');
+
+        // if office is all and name is all, fade everything in.
+        } else if(selectedOffice == 'all' && nameItemActive == 'all') {
+            $(this).addClass('card-visible').removeClass('card-hidden');
+
+        // if office is all and chosen name is same as active name (button)
+        } else if(selectedOffice == 'all' && nameItemActive == $(this).data('name')) {
+            $(this).addClass('card-visible').removeClass('card-hidden');
+        } else {
+            $(this).removeClass('card-visible').addClass('card-hidden');
+        }
+
+    });
+
+     // assign selected office to container element
+    $('.office-sort').data('active-office', selectedOffice);
+
+  });
+};
+
+EmployeeSort.prototype.sortByName = function() {
+    var _this = this;
+
+    this.$nameSelect.on('click', function(e) {
+        e.preventDefault();
+
+        // switch active class to selected button to make blue
+        $('.name-btn').removeClass('active');
+        $(this).addClass('active');
+
+        // the office button current active
+        var officeItemActive = $('.office-sort').data('active-office');
+
+        // get the name clicked item
+        var selectedName = $(this).data('name-selected');
+
+        // loop through names
+        _this.$sortItem.each(function() {
+
+            // name of card
+            var itemName = $(this).data('name');
+
+            // name of office, selected card
+            var officeName = $(this).data('office');
+
+            // if selected name matches name of card and office matches chosen office
+            if(selectedName == itemName && officeItemActive == officeName || selectedName == itemName && officeItemActive == 'all' ) {
+                $(this).addClass('card-visible').removeClass('card-hidden');
+            }
+            else if(selectedName == 'all' && officeItemActive == 'all') {
+                $(this).addClass('card-visible').removeClass('card-hidden');
+            }
+
+            // if selected name is all and office matches chosen office
+            else if(selectedName == 'all' && officeItemActive == officeName) {
+                $(this).addClass('card-visible').removeClass('card-hidden');
+            } 
+
+            // if nothing matches, fade out those items
+            else {
+                $(this).addClass('card-hidden').removeClass('card-visible');
+            }
+        });
+
+        // assign selected name to container element
+        $('.name-sort').data('active-name', selectedName);
+
+    });
+};
