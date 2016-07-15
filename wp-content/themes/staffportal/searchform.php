e4fc6800d1160e370a9bd97cabbc85615408d274
josephsap <joesap1@gmail.com>
2016-07-15 12:21:37 -0600

    ie10 fixes

--- wp-content/themes/staffportal/searchform.php
+++ wp-content/themes/staffportal/searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" id="searchform" action="/">
-    <div><label class="screen-reader-text" for="s">Search for:</label>
+    <div class="search-form-wrap"><label class="screen-reader-text" for="s">Search for:</label>
         <input type="text" value="" placeholder="Search" name="s" id="s" />
     </div>
 </form>
